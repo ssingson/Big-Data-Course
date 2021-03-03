bf761a26f9171a14d35043ab948d404eddf517b0
yingmao <giggle.my@gmail.com>
2021-03-03 17:50:28 -0500

    Update test.sh

--- mapreduce-test-python/wordcount/test.sh
+++ mapreduce-test-python/wordcount/test.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 ../../start.sh
-/usr/local/hadoop/bin/hdfs dfs -rm -r /wordcount/input/
-/usr/local/hadoop/bin/hdfs dfs -rm -r /wordcount/output/
 /usr/local/hadoop/bin/hdfs dfs -mkdir -p /wordcount/input/
 /usr/local/hadoop/bin/hdfs dfs -copyFromLocal ../../mapreduce-test-data/test.txt /wordcount/input/
 /usr/local/hadoop/bin/hadoop jar /usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-2.9.2.jar \
