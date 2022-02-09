9474d5a4637b26aa06362b8543ee16297239e253
yingmao <giggle.my@gmail.com>
2022-02-09 17:06:30 -0500

    Update test.sh

--- mapreduce-test-python/logstat2/test.sh
+++ mapreduce-test-python/logstat2/test.sh
@@ -4,7 +4,7 @@
 /usr/local/hadoop/bin/hdfs dfs -rm -r /logstat2/output/
 /usr/local/hadoop/bin/hdfs dfs -mkdir -p /logstat2/input/
 /usr/local/hadoop/bin/hdfs dfs -copyFromLocal ../../mapreduce-test-data/access.log /logstat2/input/
-/usr/local/hadoop/bin/hadoop jar /usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-2.9.2.jar \
+/usr/local/hadoop/bin/hadoop jar /usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-3.3.1.jar \
 -file ../../mapreduce-test-python/logstat2/mapper.py -mapper ../../mapreduce-test-python/logstat2/mapper.py \
 -file ../../mapreduce-test-python/logstat2/reducer.py -reducer ../../mapreduce-test-python/logstat2/reducer.py \
 -input /logstat2/input/* -output /logstat2/output/
