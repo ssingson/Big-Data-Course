03711227dd5dc138f9ad69e3134832766e852e15
Seth Singson-Robbins <seth.singson@gmail.com>
2023-02-18 15:00:55 -0500

    Update test.sh

--- Lab1/test.sh
+++ Lab1/test.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 ../../start.sh
-/usr/local/hadoop/bin/hdfs dfs -rm -r /logstat2/input/
-/usr/local/hadoop/bin/hdfs dfs -rm -r /logstat2/output/
-/usr/local/hadoop/bin/hdfs dfs -mkdir -p /logstat2/input/
-/usr/local/hadoop/bin/hdfs dfs -copyFromLocal ../../mapreduce-test-data/access.log /logstat2/input/
+/usr/local/hadoop/bin/hdfs dfs -rm -r /Lab1/input/
+/usr/local/hadoop/bin/hdfs dfs -rm -r /Lab1/output/
+/usr/local/hadoop/bin/hdfs dfs -mkdir -p /Lab1/input/
+/usr/local/hadoop/bin/hdfs dfs -copyFromLocal ../../mapreduce-test-data/access.log /Lab1/input/
 /usr/local/hadoop/bin/hadoop jar /usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-3.3.1.jar \
--file ../../mapreduce-test-python/logstat2/mapper.py -mapper ../../mapreduce-test-python/logstat2/mapper.py \
--file ../../mapreduce-test-python/logstat2/reducer.py -reducer ../../mapreduce-test-python/logstat2/reducer.py \
--input /logstat2/input/* -output /logstat2/output/
-/usr/local/hadoop/bin/hdfs dfs -cat /logstat2/output/part-00000
-/usr/local/hadoop/bin/hdfs dfs -rm -r /logstat2/input/
-/usr/local/hadoop/bin/hdfs dfs -rm -r /logstat2/output/
+-file ../../mapreduce-test-python/logstat2/mapper.py -mapper ../../Lab1/mapper.py \
+-file ../../mapreduce-test-python/logstat2/reducer.py -reducer ../../Lab1/reducer.py \
+-input /logstat2/input/* -output /Lab1/output/
+/usr/local/hadoop/bin/hdfs dfs -cat /Lab1/output/part-00000
+/usr/local/hadoop/bin/hdfs dfs -rm -r /Lab1/input/
+/usr/local/hadoop/bin/hdfs dfs -rm -r /Lab1/output/
 ../../stop.sh
