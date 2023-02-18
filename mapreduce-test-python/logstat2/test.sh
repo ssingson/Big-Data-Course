6757b99b282028c662ccf34f952e72678152f3aa
Seth Singson-Robbins <seth.singson@gmail.com>
2023-02-18 15:44:58 -0500

    Update test.sh

--- mapreduce-test-python/logstat2/test.sh
+++ mapreduce-test-python/logstat2/test.sh
@@ -10,5 +10,4 @@
 -input /logstat2/input/* -output /logstat2/output/
 /usr/local/hadoop/bin/hdfs dfs -cat /logstat2/output/part-00000
 /usr/local/hadoop/bin/hdfs dfs -rm -r /logstat2/input/
-/usr/local/hadoop/bin/hdfs dfs -rm -r /logstat2/output/
 ../../stop.sh
