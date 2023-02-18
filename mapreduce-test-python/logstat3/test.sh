46e5fb94b23f6fbf962854a201cf892a733ef2df
Seth Singson-Robbins <seth.singson@gmail.com>
2023-02-18 15:57:13 -0500

    Update test.sh

--- mapreduce-test-python/logstat3/test.sh
+++ mapreduce-test-python/logstat3/test.sh
@@ -4,7 +4,7 @@
 /usr/local/hadoop/bin/hdfs dfs -rm -r /logstat3/output/
 /usr/local/hadoop/bin/hdfs dfs -mkdir -p /logstat3/input/
 /usr/local/hadoop/bin/hdfs dfs -cp /logstat2/output/part-00000 /logstat3/input/
-/usr/local/hadoop/bin/hadoop jar /usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-2.9.2.jar \
+/usr/local/hadoop/bin/hadoop jar /usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-3.3.1.jar \
 -D mapred.reduce.tasks=0 \
 -input /logstat3/input/* -output /logstat3/output/ \
 -file ../../mapreduce-test-python/logstat3/mapper.py -mapper ../../mapreduce-test-python/logstat3/mapper.py
