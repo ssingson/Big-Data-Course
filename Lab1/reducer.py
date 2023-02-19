a52b446bc75f3d4f8fcc52749265c551553b1023
Seth Singson-Robbins <seth.singson@gmail.com>
2023-02-19 18:27:22 -0500

    Update reducer.py

--- Lab1/reducer.py
+++ Lab1/reducer.py
@@ -17,4 +17,4 @@ for line in sys.stdin:
 
 sorted_dict_ip_count = sorted(dict_ip_count.items(), key=itemgetter(0))
 for ip, count in sorted_dict_ip_count:
-    print '%s\t%s' % (ip, count)
+    print('%s\t%s' % (ip, count))
