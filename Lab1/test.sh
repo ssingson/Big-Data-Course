5e62d3def3e1d0446ebe2d53ec96b03005120c7c
Seth Singson-Robbins <seth.singson@gmail.com>
2023-02-19 23:47:16 -0500

    Update test.sh

--- Lab1/test.sh
+++ Lab1/test.sh
@@ -8,12 +8,12 @@
 /usr/local/hadoop/bin/hadoop jar /usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-3.3.1.jar \
 -file ../Lab1/mapper.py -mapper ../Lab1/mapper.py \
 -file ../Lab1/reducer.py -reducer ../Lab1/reducer.py \
--input ../Lab1/input/* -output ../Lab1/output/
+-input /Lab1/input/* -output /Lab1/output/
 
 #/usr/local/hadoop/bin/hadoop jar /usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-3.3.1.jar \
 #-file ../Lab1/mapper2.py -mapper ../Lab1/mapper2.py \
 #-file ../Lab1/reducer.py -reducer ../Lab1/reducer2.py \
-#-input ../Lab1/output/* -output ../Lab1/output2/
+#-input /Lab1/output/* -output /Lab1/output2/
 
 /usr/local/hadoop/bin/hdfs dfs -cat /Lab1/output/part-00000
 
