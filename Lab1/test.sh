44b7a661712ed5774b543d967f482897ac144f1e
Seth Singson-Robbins <seth.singson@gmail.com>
2023-02-19 23:48:11 -0500

    Update test.sh

--- Lab1/test.sh
+++ Lab1/test.sh
@@ -10,14 +10,14 @@
 -file ../Lab1/reducer.py -reducer ../Lab1/reducer.py \
 -input /Lab1/input/* -output /Lab1/output/
 
-#/usr/local/hadoop/bin/hadoop jar /usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-3.3.1.jar \
-#-file ../Lab1/mapper2.py -mapper ../Lab1/mapper2.py \
-#-file ../Lab1/reducer.py -reducer ../Lab1/reducer2.py \
-#-input /Lab1/output/* -output /Lab1/output2/
+/usr/local/hadoop/bin/hadoop jar /usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-3.3.1.jar \
+-file ../Lab1/mapper2.py -mapper ../Lab1/mapper2.py \
+-file ../Lab1/reducer.py -reducer ../Lab1/reducer2.py \
+-input /Lab1/output/* -output /Lab1/output2/
 
-/usr/local/hadoop/bin/hdfs dfs -cat /Lab1/output/part-00000
+#/usr/local/hadoop/bin/hdfs dfs -cat /Lab1/output/part-00000
 
-#/usr/local/hadoop/bin/hdfs dfs -cat /Lab1/output2/part-00000
+/usr/local/hadoop/bin/hdfs dfs -cat /Lab1/output2/part-00000
 /usr/local/hadoop/bin/hdfs dfs -rm -r /Lab1/input/
 /usr/local/hadoop/bin/hdfs dfs -rm -r /Lab1/output/
 ../stop.sh
