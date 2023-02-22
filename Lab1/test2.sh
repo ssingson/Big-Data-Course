e299ef9a5695d43795832cb8320f45ca602deec2
Seth Singson-Robbins <seth.singson@gmail.com>
2023-02-22 15:34:32 -0500

    Update test2.sh

--- Lab1/test2.sh
+++ Lab1/test2.sh
@@ -1,8 +1,17 @@
 #!/bin/sh
 ../start.sh
+/usr/local/hadoop/bin/hdfs dfs -rm -r /Lab1/input/
+/usr/local/hadoop/bin/hdfs dfs -rm -r /Lab1/output/
+/usr/local/hadoop/bin/hdfs dfs -mkdir -p /Lab1/input/
+/usr/local/hadoop/bin/hdfs dfs -copyFromLocal ../mapreduce-test-data/access.log /Lab1/input/
 
-echo 'This is a test'
-read test
+/usr/local/hadoop/bin/hadoop jar /usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-3.3.1.jar \
+-file ../Lab1/mapper_part2.py -mapper ../Lab1/mapper_part2.py \
+-file ../Lab1/reducer.py -reducer ../Lab1/reducer.py \
+-input /Lab1/input/* -output /Lab1/output/
 
-echo What is $test happening This is happening 
+/usr/local/hadoop/bin/hdfs dfs -cat /Lab1/output/part-00000
+
+/usr/local/hadoop/bin/hdfs dfs -rm -r /Lab1/input/
+/usr/local/hadoop/bin/hdfs dfs -rm -r /Lab1/output/
 ../stop.sh
