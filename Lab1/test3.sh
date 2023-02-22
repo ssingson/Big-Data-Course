97dc4f23765bc272f5ce2729dfe917f0df6c88f6
Seth Singson-Robbins <seth.singson@gmail.com>
2023-02-22 16:00:07 -0500

    Update test3.sh

--- Lab1/test3.sh
+++ Lab1/test3.sh
@@ -3,15 +3,15 @@
 
 re='^[0-9]+$'
 
-while ! [[ $yournumber =~ $re ]] || [$firsttime -gt 24] && [$firsttime -lt 0]:
-  echo What is the earliest time you want? 
-  read firsttime
-  if ! [[ $yournumber =~ $re ]] || [$firsttime -gt 24] && [$firsttime -lt 0]: 
-    echo Please insert an integer between 0 and 24. 
+#while ! [[ $yournumber =~ $re ]] || [$firsttime -gt 24] && [$firsttime -lt 0]:
+#  echo What is the earliest time you want? 
+#  read firsttime
+#  if ! [[ $yournumber =~ $re ]] || [$firsttime -gt 24] && [$firsttime -lt 0]: 
+#    echo Please insert an integer between 0 and 24. 
 
-while ! [[ $yournumber =~ $re ]] || [$lasttime -gt 24] && [$lasttime -lt 0]:
-  echo What is the latest time you want? 
-  read lasttime
+#while ! [[ $yournumber =~ $re ]] || [$lasttime -gt 24] && [$lasttime -lt 0]:
+#  echo What is the latest time you want? 
+#  read lasttime
 
-echo $firsttime $lasttime
+#echo $firsttime $lasttime
 ../stop.sh
