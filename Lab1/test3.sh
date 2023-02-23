0c6901f31b5ae874046f38eea3af534ccb6aec2b
Seth Singson-Robbins <seth.singson@gmail.com>
2023-02-22 23:33:10 -0500

    Update test3.sh

--- Lab1/test3.sh
+++ Lab1/test3.sh
@@ -9,7 +9,7 @@
 #  if ! [[ $yournumber =~ $re ]] || [$firsttime -gt 24] && [$firsttime -lt 0]: 
 #    echo Please insert an integer between 0 and 24. 
 declare -i lasttime=25
-while (($lasttime > 24)) || (($lasttime < 0)):
+while ((lasttime > 24)) || ((lasttime < 0)):
   echo 'What is the latest time you want?' 
   read lasttime
 
