f8585a7d7c35995801526fd48ce48052d32be571
Seth Singson-Robbins <seth.singson@gmail.com>
2023-02-22 23:45:00 -0500

    Update test3.sh

--- Lab1/test3.sh
+++ Lab1/test3.sh
@@ -4,7 +4,7 @@ declare -i firsttime=25
 while ((firsttime < 0)) || ((firsttime > 24))
 do
   echo 'What is the earliest hour you want?' 
-  read lasttime
+  read firsttime
   if ((firsttime < 0)) || ((firsttime > 24))
   then
     echo 'Please insert an integer between 0 and 24.' 
