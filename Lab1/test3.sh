42de925ef9eed7e7e6508fbcc814da2a8326fe55
Seth Singson-Robbins <seth.singson@gmail.com>
2023-02-23 00:47:06 -0500

    Update test3.sh

--- Lab1/test3.sh
+++ Lab1/test3.sh
@@ -8,7 +8,7 @@
 /usr/local/hadoop/bin/hdfs dfs -copyFromLocal ../mapreduce-test-data/access.log /Lab1/input/
 
 /usr/local/hadoop/bin/hadoop jar /usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-3.3.1.jar \
--file ../Lab1/mapper_part2.py -mapper ../Lab1/mapper_part2.py 3 8 \
+-file ../Lab1/mapper_part2.py -mapper '../Lab1/mapper_part2.py 3 8' \
 -file ../Lab1/reducer.py -reducer ../Lab1/reducer.py \
 -input /Lab1/input/* -output /Lab1/output/
 
