2e372d2b04d40a50829f3c0742e90bedd5a0cff4
Seth Singson-Robbins <seth.singson@gmail.com>
2023-02-23 00:11:12 -0500

    Update mapper_part2.py

--- Lab1/mapper_part2.py
+++ Lab1/mapper_part2.py
@@ -3,9 +3,11 @@
 import re
 import sys
 
+print(sysargv[1])
+print(sysargv[2])
 pat = re.compile('(?P<ip>\d+.\d+.\d+.\d+).*?\d{4}:(?P<hour>\d{2}):\d{2}.*? ')
 for line in sys.stdin:
     match = pat.search(line)
     if match:
-        if ((int(match.group('hour')) >= int(sys.argv[1])) and (int(match.group('hour')) <= int(sys.argv[2])):
+        if ((int(match.group('hour')) >= 3) and (int(match.group('hour')) <= 8):
             print('%s\t%s' % ('[' + match.group('hour') + ':00' + ']' + match.group('ip'), 1))
