fc4d7112483887d4c15cddfb6e0d5d3e568b6bab
Seth Singson-Robbins <seth.singson@gmail.com>
2023-02-22 23:48:13 -0500

    Update test2.sh

--- Lab1/test2.sh
+++ Lab1/test2.sh
@@ -1,6 +1,39 @@
 #!/bin/sh
 ../start.sh
-#test test test
+
+declare -i firsttime=25
+
+while ((firsttime < 0)) || ((firsttime > 24))
+do
+  echo 'What is the earliest hour you want?' 
+  read firsttime
+  if ((firsttime < 0)) || ((firsttime > 24))
+  then
+    echo 'Please insert an integer between 0 and 24.' 
+  fi
+done
+echo 
+
+# Set the latest hour for the filter
+declare -i lasttime=25
+
+
+while ((lasttime < 0)) || ((lasttime > 24))
+do
+  echo 'What is the latest hour you want?' 
+  read lasttime
+  if ((lasttime < 0)) || ((lasttime > 24))
+  then
+    echo 'Please insert an integer between 0 and 24.' 
+  fi
+done
+echo
+
+#print earliest and latest time
+echo The top IPs will be between $firsttime:00 and $lasttime:00.
+echo 
+echo Test
+
 /usr/local/hadoop/bin/hdfs dfs -rm -r /Lab1/input/
 /usr/local/hadoop/bin/hdfs dfs -rm -r /Lab1/output/
 /usr/local/hadoop/bin/hdfs dfs -rm -r /Lab1/output2/
@@ -8,7 +41,7 @@
 /usr/local/hadoop/bin/hdfs dfs -copyFromLocal ../mapreduce-test-data/access.log /Lab1/input/
 
 /usr/local/hadoop/bin/hadoop jar /usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-3.3.1.jar \
--file ../Lab1/mapper.py -mapper ../Lab1/mapper.py \
+-file ../Lab1/mapper.py $firsttime $lasttime -mapper ../Lab1/mapper.py $firsttime $lasttime \
 -file ../Lab1/reducer.py -reducer ../Lab1/reducer.py \
 -input /Lab1/input/* -output /Lab1/output/
 
