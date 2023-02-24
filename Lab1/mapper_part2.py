aad43cea6960354ea2eaad69a293af9236c91255
Seth Singson-Robbins <seth.singson@gmail.com>
2023-02-23 19:38:59 -0500

    Update mapper_part2.py

--- Lab1/mapper_part2.py
+++ Lab1/mapper_part2.py
@@ -3,9 +3,11 @@
 import re
 import sys
 
+# Maps the data pulling the hour and IP address and having a count of 1 for later reducing.  
 pat = re.compile('(?P<ip>\d+.\d+.\d+.\d+).*?\d{4}:(?P<hour>\d{2}):\d{2}.*? ')
 for line in sys.stdin:
     match = pat.search(line)
-    if match:
+    if match:   
+        #Based on a time range inputed by the user, filters out only data that is within the time range. 
         if ((int(match.group('hour')) >= int(sys.argv[1])) and (int(match.group('hour')) <= int(sys.argv[2]))):
             print('%s\t%s' % ('[' + match.group('hour') + ':00' + ']' + match.group('ip'), 1))
