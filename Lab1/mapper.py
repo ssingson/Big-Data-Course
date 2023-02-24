ed395414bd4a56beafa80b5c5503b762480ab25e
Seth Singson-Robbins <seth.singson@gmail.com>
2023-02-23 19:37:36 -0500

    Update mapper.py

--- Lab1/mapper.py
+++ Lab1/mapper.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python
 # --*-- coding:utf-8 --*--
+
 import re
 import sys
 
+# Maps the data pulling the hour and IP address and having a count of 1 for later reducing.  
 pat = re.compile('(?P<ip>\d+.\d+.\d+.\d+).*?\d{4}:(?P<hour>\d{2}):\d{2}.*? ')
 for line in sys.stdin:
     match = pat.search(line)
