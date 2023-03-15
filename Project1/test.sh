7212c983af783391878031dcf4b82fd43e6d7bd7
Seth Singson-Robbins <seth.singson@gmail.com>
2023-03-15 17:01:07 -0500

    Update test.sh

--- Project1/test.sh
+++ Project1/test.sh
@@ -5,7 +5,7 @@
 /usr/local/hadoop/bin/hdfs dfs -rm -r /output/
 /usr/local/hadoop/bin/hdfs dfs -rm -r /output2/
 /usr/local/hadoop/bin/hdfs dfs -mkdir -p /input/
-/usr/local/hadoop/bin/hdfs dfs -copyFromLocal parking_sampled.csv /Lab1/input/
+/usr/local/hadoop/bin/hdfs dfs -copyFromLocal parking_sampled.csv /input/
 /usr/local/hadoop/bin/hadoop jar /usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-3.3.1.jar \
 -file /mapper.py -mapper /mapper.py \
 -file /reducer.py -reducer /reducer.py \
