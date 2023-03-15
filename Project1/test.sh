425706543918cf8ca3bd90f13ce66d2477f71db8
Seth Singson-Robbins <seth.singson@gmail.com>
2023-03-15 16:28:39 -0500

    Update test.sh

--- Project1/test.sh
+++ Project1/test.sh
@@ -5,8 +5,7 @@
 /usr/local/hadoop/bin/hdfs dfs -rm -r /output/
 /usr/local/hadoop/bin/hdfs dfs -rm -r /output2/
 /usr/local/hadoop/bin/hdfs dfs -mkdir -p /input/
-/usr/local/hadoop/bin/hdfs dfs -copyFromLocal ../mapreduce-test-data/access.log /input/
-
+curl -o /input/parking_violations.csv https://data.cityofnewyork.us/api/views/pvqr-7yc4/rows.csv?accessType=DOWNLOAD
 /usr/local/hadoop/bin/hadoop jar /usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-3.3.1.jar \
 -file /mapper.py -mapper /mapper.py \
 -file /reducer.py -reducer /reducer.py \
