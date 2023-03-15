51fcf8d5e80be3cad9902dd3809b0e7920ff3f9f
Seth Singson-Robbins <seth.singson@gmail.com>
2023-03-15 17:57:18 -0500

    Update mapper.py

--- Project1/mapper.py
+++ Project1/mapper.py
@@ -8,7 +8,7 @@ lines = [s.split(',') for s in sys.stdin]
 columns = lines[0]
 columns.append('Extra Line')
 
-df = pd.DataFrame(data = lines, columns = columns)
+df = pd.DataFrame(data = lines[1:], columns = columns)
 
 for index, row in df.iterrows():
    print('%s\t%s' % ( random.randint(1,3), (row['Street Code1'], row['Street Code2'],row['Street Code3'])))
