e8f78f22faa0b965a61ffdfb6e26d081da28048d
Seth Singson-Robbins <seth.singson@gmail.com>
2023-03-15 19:24:39 -0500

    Delete Project 1 directory

--- Project 1/test.py
+++
@@ -1,8 +0,0 @@
-import os
-
-# set the variable name and value
-variable_name = 'MY_VARIABLE'
-variable_value = 'some_value'
-
-# set the environment variable
-os.environ[variable_name] = variable_value
