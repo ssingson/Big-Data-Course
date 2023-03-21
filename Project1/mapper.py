2f691a2ca5e640f5a2c464b46fdf0ea20aaaa8f9
Seth Singson-Robbins <seth.singson@gmail.com>
2023-03-20 22:07:34 -0500

    Update mapper.py

--- Project1/mapper.py
+++ Project1/mapper.py
@@ -8,3 +8,5 @@ for line in sys.stdin:
    try: 
       line = line.strip().split(",")
       print('%s\t%s' % (random.randint(1,3), '(' + line[10] + ',' + line[11] + ',' + line[12] + ')'))
+   except: 
+      pass
