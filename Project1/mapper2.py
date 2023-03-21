13a0d6f0ae1dce66c2c89c7dad6d54ba041c8bdb
Seth Singson-Robbins <seth.singson@gmail.com>
2023-03-20 22:32:57 -0500

    Update mapper2.py

--- Project1/mapper2.py
+++ Project1/mapper2.py
@@ -7,22 +7,24 @@ import pandas as pd
 
 centroids = []
 
-for line in sys.stdin[0]: 
+for line in sys.stdin[1]: 
     line = [float(x) for x in line.strip('(').strip('\n').strip(')').split(',')]
     centroids.append(line)
-
-lines = [s.split(',') for s in sys.stdin[1]]
-columns = lines[0]
-columns.append('Extra Line')
-
-df = pd.DataFrame(data = lines[1:], columns = columns)
-
-for index, row in df.iterrows():
-    min_distance = float('inf')
-    centroid = 0
-    for i in range(len(centroids)): 
-      distance = abs(int(row['Street Code1']) - int(centroids[i][0])) + abs(int(row['Street Code2']) - int(centroids[i][1])) + abs(int(row['Street Code3']) - int(centroids[i][2]))
-      if distance < min_distance: 
-        min_distance = distance
-        centroid = i
-    print('%s\t%s' % ( centroids[centroid], (row['Street Code1'], row['Street Code2'],row['Street Code3'])))
+    
+n = 0
+for line in sys.stdin[0]: 
+   if n == 0:
+      n += 1
+   else:
+      try: 
+        min_distance = float('inf')
+        centroid = 0
+        for i in range(len(centroids)): 
+          distance = abs(int(row['Street Code1']) - int(centroids[i][0])) + abs(int(row['Street Code2']) - int(centroids[i][1])) + abs(int(row['Street Code3']) - int(centroids[i][2]))
+          if distance < min_distance: 
+            min_distance = distance
+            centroid = i
+            line = line.strip().split(",")
+         print('%s\t%s' % (random.randint(1,3), '(' + line[10] + ',' + line[11] + ',' + line[12] + ')'))
+      except ValueError: 
+         pass
