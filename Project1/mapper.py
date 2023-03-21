dea2ee999ad2ee4e9d4fb163b3bd38c9a2a58d48
Seth Singson-Robbins <seth.singson@gmail.com>
2023-03-20 22:10:30 -0500

    Update mapper.py

--- Project1/mapper.py
+++ Project1/mapper.py
@@ -4,9 +4,13 @@
 import re
 import sys
 
+n = 0
 for line in sys.stdin: 
-   try: 
-      line = line.strip().split(",")
-      print('%s\t%s' % (random.randint(1,3), '(' + line[10] + ',' + line[11] + ',' + line[12] + ')'))
-   except ValueError: 
-      pass
+   if n == 0:
+      n += 1
+   else:
+      try: 
+         line = line.strip().split(",")
+         print('%s\t%s' % (random.randint(1,3), '(' + line[10] + ',' + line[11] + ',' + line[12] + ')'))
+      except ValueError: 
+         pass
