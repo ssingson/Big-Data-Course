43947dee485ec9d8da98eaff78c3ea9e0b9b47a3
Seth Singson-Robbins <seth.singson@gmail.com>
2023-03-20 21:56:57 -0500

    Update mapper.py

--- Project1/mapper.py
+++ Project1/mapper.py
@@ -7,4 +7,4 @@ import sys
 for line in sys.stdin: 
    try: 
       line = line.strip().split(",")
-      print('%s\t%s' % (random.randint(1,3), (line[10], line[11],line[12])))
+      print('%s\t%s' % (random.randint(1,3), '(' + line[10] + ',' + line[11] + ',' + line[12] + ')'))
