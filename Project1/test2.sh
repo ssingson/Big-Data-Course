a8947ee8daff82e2e1bc6ce08edb61d4b395c90c
Seth Singson-Robbins <seth.singson@gmail.com>
2023-03-22 15:15:45 -0500

    Update test2.sh

--- Project1/test2.sh
+++ Project1/test2.sh
@@ -18,7 +18,10 @@
 /usr/local/hadoop/bin/hadoop jar /usr/local/hadoop/share/hadoop/tools/lib/hadoop-streaming-3.3.1.jar \
 -file mapper2.py -mapper mapper2.py \
 -file reducer.py -reducer reducer.py \
--input /input/* /output2/part-00000 -output /output2/
+-input /input/* \
+-input /output/* \
+-inputformat org.apache.hadoop.mapred.TextInputFormat \
+-output /output2/
 
 /usr/local/hadoop/bin/hdfs dfs -cat /output2/part-00000
 /usr/local/hadoop/bin/hdfs dfs -rm -r /input/
