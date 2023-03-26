8838faa61b5fbd10a275dc6d564dd8b44f6c81bd
Seth Singson-Robbins <seth.singson@gmail.com>
2023-03-26 16:32:41 -0400

    Update mapper - black car.py

--- Project1/mapper - black car.py
+++ Project1/mapper - black car.py
@@ -15,7 +15,15 @@ for line in sys.stdin:
         line = [float(x) for x in line.strip('(').strip('\n').strip(')').split(',')]
         centroids.append(line)
     
-
+#find centroid of the location specified
+for i in range(len(centroids)): 
+  min_distance = float('inf')
+  main_centroid = 0                                                                          
+  distance = ((34510 - int(centroids[i][0]))) ** 2 + ((10030 - int(centroids[i][1]))) ** 2 + ((34050 - int(centroids[i][2]))) ** 2) ** 0.5
+  if distance < min_distance: 
+    min_distance = distance
+    main_centroid = i
+    
 #Only counts black cars
 n = 0
 for line in sys.stdin:
@@ -34,25 +42,15 @@ for line in sys.stdin:
                   distance = ((int(line[10] - int(centroids[i][0]))) ** 2 + ((int(line[11] - int(centroids[i][1]))) ** 2 + ((int(line[12] - int(centroids[i][2]))) ** 2) ** 0.5
                   if distance < min_distance: 
                     min_distance = distance
-                    centroid = i
-                 centroids_count[centroid] = centroids_count[centroid] + 1                                                           
+                    centroid = i 
+                  # returns 1 if it's in the car's centroid, 0 otherwise                                                          
+                  if main_centroid == centroid:
+                    print('%s\t%s' % ('Black Car', str(1)))
+                  else:                      
+                    print('%s\t%s' % ('Black Car', str(0)))                                                   
               except ValueError: 
                  pass
 
-#find centroid of the location specified
-for i in range(len(centroids)): 
-  min_distance = float('inf')
-  centroid = 0                                                                          
-  distance = ((34510 - int(centroids[i][0]))) ** 2 + ((10030 - int(centroids[i][1]))) ** 2 + ((34050 - int(centroids[i][2]))) ** 2) ** 0.5
-  if distance < min_distance: 
-    min_distance = distance
-    centroid = i
 
-#pushes all under 'Black Car' so in one group, each push the cluster's number of black cars and if this cluster is where the desired location is in                              
-for i in len(centroids): 
-  if i == centroid:
-    print('%s\t%s' % ('Black Car', '(' + str(centroids_count[i]) + ',' + str(1) + ')'))
-  else:                      
-    print('%s\t%s' % ('Black Car', '(' + str(centroids_count[i]) + ',' + str(0) + ')'))
 
                                                                             
