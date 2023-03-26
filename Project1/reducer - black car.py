c6cf1f0d0831f43e21f21e68a52dfc5a88c73e88
Seth Singson-Robbins <seth.singson@gmail.com>
2023-03-26 16:33:33 -0400

    Update reducer - black car.py

--- Project1/reducer - black car.py
+++ Project1/reducer - black car.py
@@ -2,15 +2,16 @@
 
 
 car_count = 0
+cluster_count = 0
 
 for line in sys.stdin:
     line = line.strip()
     group, counts = line.split('\t')
-    streets = [int(x.strip(" '")) for x in streets.strip('(').strip(')').split(',')]
+    streets = [int(x.strip(" '")) for x in streets]
     try: 
-      car_count = car_count + streets[0]
+      car_count = car_count + 1
       if streets[1] == 1:
-        cluster_count = streets[0]
+        cluster_count = cluster_count + 1
 
 print('The likelihood of a black car getting a ticket at 34510, 10030, 34050 is ' + str(cluster_count / car_count) + '.')
     
