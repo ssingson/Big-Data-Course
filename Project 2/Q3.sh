fdeeab599c0d2a25c9bf0940e05419be8015178c
Jingyan Xu <76541511+horatioxu1122@users.noreply.github.com>
2023-05-02 13:49:25 -0400

    Update Q3.sh

--- Project 2/Q3.sh
+++ Project 2/Q3.sh
@@ -5,7 +5,7 @@ hadoop dfsadmin -safemode leave
 /usr/local/hadoop/bin/hdfs dfs -rm -r /P2/Part3/input/
 /usr/local/hadoop/bin/hdfs dfs -rm -r /P2/Part3/output/
 /usr/local/hadoop/bin/hdfs dfs -mkdir -p /P2/Part3/input/
-/usr/local/hadoop/bin/hdfs dfs -copyFromLocal train.csv /P2/Part3/input/
+/usr/local/hadoop/bin/hdfs dfs -copyFromLocal training.csv /P2/Part3/input/
 /usr/local/hadoop/bin/hdfs dfs -copyFromLocal test.csv /P2/Part3/input/
 /usr/local/spark/bin/spark-submit --master=spark://$SPARK_MASTER:7077 Q3.py hdfs://$SPARK_MASTER:9000/P2/Part3/input/
 ../../stop.sh
